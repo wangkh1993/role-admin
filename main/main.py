7eacfbe28b5ebe714595fcc70f154ea639512506
Jayden <wangkh19930128@gmail.com>
2021-11-27 15:59:35 +1100

    Init main.py

---
+++ main/main.py
@@ -0,0 +1,33 @@
+
+
+
+roles = [
+    {"Id": 1, "Name": "System Administrator", "Parent": 0},
+    {
+        "Id": 2,
+        "Name": "Location Manager",
+        "Parent": 1,
+    },
+    {
+        "Id": 3,
+        "Name": "Supervisor",
+        "Parent": 2,
+    },
+    {
+        "Id": 4,
+        "Name": "Employee",
+        "Parent": 3,
+    },
+    {
+        "Id": 5,
+        "Name": "Trainer",
+        "Parent": 3,
+    },
+]
+users = [
+    {"Id": 1, "Name": "Adam Admin", "Role": 1},
+    {"Id": 2, "Name": "Emily Employee", "Role": 4},
+    {"Id": 3, "Name": "Sam Supervisor", "Role": 3},
+    {"Id": 4, "Name": "Mary Manager", "Role": 2},
+    {"Id": 5, "Name": "Steve Trainer", "Role": 5},
+]
